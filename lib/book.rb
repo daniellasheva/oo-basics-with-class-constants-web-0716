ddf5c8d040e42969a9b5e5e28ddae1841493fa25
daniellasheva <daniellasheva@gmail.com>
2016-07-28 10:29:42 -0400

    initial commit

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,20 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   attr_reader :title
-
+  
   def initialize(title)
-    @title = title
+      @title = title
   end
+  
+  GENRES= []
+  
+  def genre=(genre)
+    @genre=genre
+    GENRES<<genre  #This pushes that new genre into our genre [] so we can access it later
+  end
+  
+  attr_reader :genre
+  
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
